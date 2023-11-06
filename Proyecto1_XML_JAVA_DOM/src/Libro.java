e5890f2815314f03a7378f7bd384ece38aabcd8c
Almorsan <131298499+Almorsan@users.noreply.github.com>
2023-11-06 21:17:56 +0100

    Cuarto commit

---
+++ Proyecto1_XML_JAVA_DOM/src/Libro.java
@@ -0,0 +1,78 @@
+
+
+public class Libro {
+    
+    String id, autor, titulo, genero, fecha, descripcion;
+    double precio;
+
+    public Libro(String id, String autor, String titulo, String genero, String fecha, String descripcion, double precio) {
+        this.id = id;
+        this.autor = autor;
+        this.titulo = titulo;
+        this.genero = genero;
+        this.fecha = fecha;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    
+
+    
+    
+}
--- Proyecto1_XML_JAVA_DOM/src/MetodosDOMyXML.java
+++ Proyecto1_XML_JAVA_DOM/src/MetodosDOMyXML.java
@@ -6,6 +6,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
@@ -88,6 +89,101 @@ public class MetodosDOMyXML {
             }
             
         }
+        
+    }
+    
+    
+    public int insertarLibroEnDom(Libro unLibro) {
+        
+        try {
+            System.out.println("Añaiendo al árbol el siguiente libro: \n");
+            System.out.println("Título: " +unLibro.getTitulo()+ "\n");
+            System.out.println("id: " +unLibro.getId() + "\n");
+            System.out.println("Autor: " +unLibro.getAutor() + "\n");
+            System.out.println("Género: " +unLibro.getGenero() + "\n");
+            System.out.println("Precio: " +unLibro.getPrecio() + "\n");
+            System.out.println("Fecha de publicación: " +unLibro.getFecha()+ "\n");
+            System.out.println("Descripción: " + unLibro.getDescripcion() + "\n");
+            System.out.println("--------");
+            System.out.println("");
+            
+            
+            
+             //Añadimos el autor
+        Node nodoAutor=documento.createElement("author");
+        Node nodoAutor_text=documento.createTextNode(unLibro.getAutor());
+        nodoAutor.appendChild(nodoAutor_text);
+        
+        
+        
+            //Añadimos el título 
+            
+        Node nodoTitulo=documento.createElement("title");
+        Node nodoTitulo_text=documento.createTextNode(unLibro.getTitulo());
+        nodoTitulo.appendChild( nodoTitulo_text);
+            
+            
+            //Añadimos el género
+        Node nodoGenero=documento.createElement("genre");
+        Node nodoGenero_text=documento.createTextNode(unLibro.getGenero());
+        nodoGenero.appendChild(  nodoGenero_text);
+            
+            
+            //Añadimos el precio
+            
+            //para eso,  parseamos el double del pecio a String
+            
+            String precio = String.valueOf(unLibro.getPrecio());
+        
+        Node nodoPrecio=documento.createElement("price");
+        Node nodoPrecio_text=documento.createTextNode(precio);
+        nodoPrecio.appendChild(  nodoPrecio_text);
+            
+        
+       
+            //Añadimos la fecha de publicación
+            
+        Node nodoFecha=documento.createElement("publish_date");
+        Node nodoFecha_text=documento.createTextNode(unLibro.getFecha());
+        nodoFecha.appendChild(  nodoFecha_text);
+            
+            
+            //Añadimos la descipción
+            
+        Node nodoDescripcion=documento.createElement("publish_date");
+        Node nodoDescripcion_text=documento.createTextNode(unLibro.getDescripcion());
+        nodoDescripcion.appendChild(  nodoDescripcion_text);
+            
+            
+            //Añadimos el libro con el atributo id
+            
+          Node nodoLibro=documento.createElement("book");
+          ((Element)nodoLibro).setAttribute("id",unLibro.getId());
+          nodoLibro.appendChild(nodoAutor);
+          nodoLibro.appendChild(nodoTitulo);
+          nodoLibro.appendChild(nodoGenero);
+          nodoLibro.appendChild(nodoPrecio);
+          nodoLibro.appendChild(nodoFecha);
+          nodoLibro.appendChild(nodoDescripcion);
+          
+          
+          nodoLibro.appendChild(documento.createTextNode("\n"));
+          Node raiz=documento.getFirstChild();//tb. doc.getChildNodes().item(0)
+          raiz.appendChild(nodoLibro);
+          System.out.println("Libro insertado en DOM.");
+        
+        
+            
+            
+            
+          return 0;  
+        } catch (Exception e) {
+            System.out.println("Error, no se ha podido insertar el libro"+e);
+            return -1; 
+           }
+        
+        
+        
         
     }
     
